2f4c1ab2e47142f6cd369a235c55ddbe4e141efa
mzltest <52124882+mzltest@users.noreply.github.com>
2023-04-15 07:57:09 +0000

    heh

--- todos/delete/index.js
+++ todos/delete/index.js
@@ -4,6 +4,6 @@ let data = require('@begin/data')
 exports.handler = async function destroy(req) {
   let b = arc.http.helpers.bodyParser(req)
   key=b.channel
-  dt=await data.set({'table':todos,'key':key,'updated':Date.now(),'command':'','output':b.output})
+  dt=await data.set({'table':'todos','key':key,'updated':Date.now(),'command':'','output':b.output})
   return {'ok':true}
 }
