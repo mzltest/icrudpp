fc3bea0a05592775c8e3e900189b8b9c52b584c5
mzltest <52124882+mzltest@users.noreply.github.com>
2023-04-15 07:17:41 +0000

    ssss

--- todos/read/index.js
+++ todos/read/index.js
@@ -3,5 +3,7 @@ const data = require('@begin/data')
 exports.handler = async function read(req) {
   channel=req.pathParameters.channel
   dt=await data.get({'table':'todos','key':channel}) 
+  dt.updated = Date.now()
+  await data.set(dt)
   return {'command':dt.command}
 }
